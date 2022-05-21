69b59c0bef3610f480477bea4d1d513f70342ab9
scottwe <ar627383@dal.ca>
2022-05-21 08:11:40 -0400

    Fixed test suite setup.
    Failed tests would not fail `cabal test`. This is because cabal expected the tests to return 1 on failure.
    Solved by using the `Test.Framework.Providers.HUnit` framework.

--- tools/test/DyadicRewrite-Rewrite-Rules-Tests.hs
+++ tools/test/DyadicRewrite-Rewrite-Rules-Tests.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Test.Framework
+import Test.Framework.Providers.HUnit
 import Test.HUnit
 import DyadicRewrite.Common
 import DyadicRewrite.Rewrite.Rules
@@ -34,32 +36,26 @@ k12 = Gate "K" [1, 2]
 -----------------------------------------------------------------------------------------
 -- Tests when a rewrite rule is applicable (in either direction).
 
-circuit1a :: Circuit
-circuit1a = [ccx123, cx13, cx23]
+circ1a :: Circuit
+circ1a = [ccx123, cx13, cx23]
 
-circuit1b :: Circuit
-circuit1b = [cx13, ccx123, cx23]
+circ1b :: Circuit
+circ1b = [cx13, ccx123, cx23]
 
 rule1 :: RewriteRule
 rule1 = RewriteRule [ccx123, cx13] [cx13, ccx123]
 
-test1 :: Test
-test1 = TestCase (assertBool "Can apply CCX123.CX13 = CX13.CCX123 to CCX123.CX13.CX23"
-                             (checkRewriteRule circuit1a rule1 True))
+test1 = TestCase $ assertBool "Can apply CCX123.CX13 = CX13.CCX123 to CCX123.CX13.CX23"
+                              (checkRewriteRule circ1a rule1 True)
 
-test2 :: Test
-test2 = TestCase (assertBool "Can apply CCX123.CX13 = CX13.CCX123 to CX13.CCX123.CX23"
-                             (checkRewriteRule circuit1b rule1 False))
+test2 = TestCase $ assertBool "Can apply CCX123.CX13 = CX13.CCX123 to CX13.CCX123.CX23"
+                              (checkRewriteRule circ1b rule1 False)
 
-test3 :: Test
-test3 = TestCase (assertEqual "CCX123.CX13.CX23 =R1=> CX13.CCX123.CX23"
-                              circuit1b
-                              (applyRewriteRule circuit1a rule1 True))
+test3 = TestCase $ assertEqual "CCX123.CX13.CX23 =R1=> CX13.CCX123.CX23"
+                               circ1b (applyRewriteRule circ1a rule1 True)
 
-test4 :: Test
-test4 = TestCase (assertEqual "CX13.CCX123.CX23 =R1=> CCX123.CX13.CX23"
-                              circuit1a
-                              (applyRewriteRule circuit1b rule1 False))
+test4 = TestCase $ assertEqual "CX13.CCX123.CX23 =R1=> CCX123.CX13.CX23"
+                               circ1a (applyRewriteRule circ1b rule1 False)
 
 -----------------------------------------------------------------------------------------
 -- Tests when a rewrite rule is not applicable.
@@ -70,22 +66,20 @@ rule2 = RewriteRule [ccx123, cx13, cx23, cx12] [ccx123, cx13, cx23, cx12]
 rule3 :: RewriteRule
 rule3 = RewriteRule [ccx123, cx23] [cx23, ccx123]
 
-test5 :: Test
-test5 = TestCase (assertBool "The rule is too long and must be rejected"
-                             (not (checkRewriteRule circuit1a rule2 True)))
+test5 = TestCase $ assertBool "The rule is too long and must be rejected"
+                              (not (checkRewriteRule circ1a rule2 True))
 
-test6 :: Test
-test6 = TestCase (assertBool "The rule does not match and must be rejected"
-                             (not (checkRewriteRule circuit1a rule3 True)))
+test6 = TestCase $ assertBool "The rule does not match and must be rejected"
+                              (not (checkRewriteRule circ1a rule3 True))
 
 -----------------------------------------------------------------------------------------
 -- Tests when a rewrite operation is applicable (in either direction).
 
-circuit2a :: Circuit
-circuit2a = [x1, x2, z1, ccx123, cx13, cx23]
+circ2a :: Circuit
+circ2a = [x1, x2, z1, ccx123, cx13, cx23]
 
-circuit2b :: Circuit
-circuit2b = [x1, x2, z1, cx13, ccx123, cx23]
+circ2b :: Circuit
+circ2b = [x1, x2, z1, cx13, ccx123, cx23]
 
 op1a :: RewriteOp
 op1a = RewriteOp rule1 3 True
@@ -93,31 +87,23 @@ op1a = RewriteOp rule1 3 True
 op1b :: RewriteOp
 op1b = RewriteOp rule1 3 False
 
-test7 :: Test
-test7 = TestCase (assertBool "Can apply CCX123.CX13 = CX13.CCX123 forward at index 3"
-                             (checkRewriteOp circuit2a op1a))
+test7 = TestCase $ assertBool "Can apply CCX123.CX13 = CX13.CCX123 forward at index 3"
+                              (checkRewriteOp circ2a op1a)
 
-test8 :: Test
-test8 = TestCase (assertBool "Can apply CCX123.CX13 = CX13.CCX123 backwards at index 3"
-                             (checkRewriteOp circuit2b op1b))
+test8 = TestCase $ assertBool "Can apply CCX123.CX13 = CX13.CCX123 backwards at index 3"
+                              (checkRewriteOp circ2b op1b)
 
-test9 :: Test
-test9 = TestCase (assertEqual
-                  "X1.X2.Z1.CCX123.CX13.CX23 =(R1 at 3)=> X1.X2.Z1.CX13.CCX123.CX23"
-                  circuit2b
-                  (applyRewriteOp circuit2a op1a))
+test9 = TestCase $ assertEqual "(...).CCX123.CX13.CX23 =(R1@3)=> (...).CX13.CCX123.CX23"
+                   circ2b (applyRewriteOp circ2a op1a)
 
-test10 :: Test
-test10 = TestCase (assertEqual
-                   "X1.X2.Z1.CX13.CCX123.CX23 =(R1 at 3)=> X1.X2.Z1.CCX123.CX13.CX23"
-                   circuit2a
-                   (applyRewriteOp circuit2b op1b))
+test10 = TestCase $ assertEqual "(...).CX13.CCX123.CX23 =(R1@3)=> (...).CCX123.CX13.CX23"
+                    circ2a (applyRewriteOp circ2b op1b)
 
-circuit3a :: Circuit
-circuit3a = [k12, x1, x2, z1, ccx123, cx13, cx23]
+circ3a :: Circuit
+circ3a = [k12, x1, x2, z1, ccx123, cx13, cx23]
 
-circuit3b :: Circuit
-circuit3b = [k12, x1, x2, z1, cx13, ccx123, cx23]
+circ3b :: Circuit
+circ3b = [k12, x1, x2, z1, cx13, ccx123, cx23]
 
 op2a :: RewriteOp
 op2a = RewriteOp rule1 4 True
@@ -125,68 +111,54 @@ op2a = RewriteOp rule1 4 True
 op2b :: RewriteOp
 op2b = RewriteOp rule1 4 False
 
-test11 :: Test
-test11 = TestCase (assertBool "Can apply CCX123.CX13 = CX13.CCX123 forward at index 4"
-                             (checkRewriteOp circuit3a op2a))
+test11 = TestCase $ assertBool "Can apply CCX123.CX13 = CX13.CCX123 forward at index 4"
+                               (checkRewriteOp circ3a op2a)
 
-test12 :: Test
-test12 = TestCase (assertBool "Can apply CCX123.CX13 = CX13.CCX123 backwards at index 4"
-                             (checkRewriteOp circuit3b op2b))
+test12 = TestCase $ assertBool "Can apply CCX123.CX13 = CX13.CCX123 backwards at index 4"
+                               (checkRewriteOp circ3b op2b)
 
-test13 :: Test
-test13 = TestCase (assertEqual
-                   "K12.X1.X2.Z1.CCX123.CX13.CX23 =(R1 at 4)=> X1.X2.Z1.CX13.CCX123.CX23"
-                   circuit3b
-                   (applyRewriteOp circuit3a op2a))
+test13 = TestCase $ assertEqual "(...).CCX123.CX13.CX23 =(R1@4)=> (...).CX13.CCX123.CX23"
+                    circ3b (applyRewriteOp circ3a op2a)
 
-test14 :: Test
-test14 = TestCase (assertEqual
-                   "K12.X1.X2.Z1.CX13.CCX123.CX23 =(R1 at 4)=> X1.X2.Z1.CCX123.CX13.CX23"
-                   circuit3a
-                   (applyRewriteOp circuit3b op2b))
+test14 = TestCase $ assertEqual "(...).CX13.CCX123.CX23 =(R1@4)=> (...).CCX123.CX13.CX23"
+                    circ3a (applyRewriteOp circ3b op2b)
 
 -----------------------------------------------------------------------------------------
 -- Tests edge cases with empty strings.
 
-circuit4a :: Circuit
-circuit4a = [ccx123, ccx123]
+circ4a :: Circuit
+circ4a = [ccx123, ccx123]
 
-circuit4b :: Circuit
-circuit4b = []
+circ4b :: Circuit
+circ4b = []
 
 rule4 :: RewriteRule
 rule4 = RewriteRule [ccx123, ccx123] []
 
-test15 :: Test
-test15 = TestCase (assertEqual "Can support rules that eliminate symbols"
-                               circuit4b
-                               (applyRewriteRule circuit4a rule4 True))
+test15 = TestCase $ assertEqual "Can support rules that eliminate symbols"
+                                circ4b (applyRewriteRule circ4a rule4 True)
 
-test16 :: Test
-test16 = TestCase (assertEqual "Can support rules that introduce symbols"
-                               circuit4a
-                               (applyRewriteRule circuit4b rule4 False))
+test16 = TestCase $ assertEqual "Can support rules that introduce symbols"
+                                circ4a (applyRewriteRule circ4b rule4 False)
 
 -----------------------------------------------------------------------------------------
 -- Orchestrates tests.
 
-tests :: Test
-tests = TestList [TestLabel "CheckRuleForwards" test1,
-                  TestLabel "CheckRuleBackwards" test2,
-                  TestLabel "ApplyRuleForwards" test3,
-                  TestLabel "ApplyRuleBackwards" test4,
-                  TestLabel "RuleTooLong" test5,
-                  TestLabel "RuleDoesNotMatch" test6,
-                  TestLabel "CheckOpAt3Forwards" test7,
-                  TestLabel "CheckOpAt3Backwards" test8,
-                  TestLabel "ApplyOpAt3Forwards" test9,
-                  TestLabel "ApplyOpAt3Backwards" test10,
-                  TestLabel "CheckOpAt4Forwards" test11,
-                  TestLabel "CheckOpAt4Backwards" test12,
-                  TestLabel "ApplyOpAt4Forwards" test13,
-                  TestLabel "ApplyOpAt4Backwards" test14,
-                  TestLabel "ApplyElimination" test15,
-                  TestLabel "ApplyIntroduce" test16]
+tests = hUnitTestToTests $ TestList [TestLabel "CheckRuleForwards" test1,
+                                     TestLabel "CheckRuleBackwards" test2,
+                                     TestLabel "ApplyRuleForwards" test3,
+                                     TestLabel "ApplyRuleBackwards" test4,
+                                     TestLabel "RuleTooLong" test5,
+                                     TestLabel "RuleDoesNotMatch" test6,
+                                     TestLabel "CheckOpAt3Forwards" test7,
+                                     TestLabel "CheckOpAt3Backwards" test8,
+                                     TestLabel "ApplyOpAt3Forwards" test9,
+                                     TestLabel "ApplyOpAt3Backwards" test10,
+                                     TestLabel "CheckOpAt4Forwards" test11,
+                                     TestLabel "CheckOpAt4Backwards" test12,
+                                     TestLabel "ApplyOpAt4Forwards" test13,
+                                     TestLabel "ApplyOpAt4Backwards" test14,
+                                     TestLabel "ApplyElimination" test15,
+                                     TestLabel "ApplyIntroduce" test16]
 
-main :: IO Counts
-main = runTestTT tests
+main = defaultMain tests
--- tools/test/DyadicRewrite-Rewrite-Simplification-Tests.hs
+++ tools/test/DyadicRewrite-Rewrite-Simplification-Tests.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Test.Framework
+import Test.Framework.Providers.HUnit
 import Test.HUnit
 import DyadicRewrite.Common
 import DyadicRewrite.Rewrite.Rules
@@ -35,26 +37,20 @@ k12 = Gate "K" [1, 2]
 -----------------------------------------------------------------------------------------
 -- Edge cases: success.
 
-test1 :: Test
-test1 = TestCase (assertEqual "Support for empty lists of operations on empty circuits"
-                              (RewriteResult [] 0 True)
-                              (simplify [] []))
+test1 = TestCase $ assertEqual "Support for empty lists of operations on empty circuits"
+                               (RewriteResult [] 0 True) (simplify [] [])
 
-circuit1 :: Circuit
-circuit1 = [x1, x1]
+circ1 :: Circuit
+circ1 = [x1, x1]
 
-test2 :: Test
-test2 = TestCase (assertEqual "Support for empty lists of operations"
-                              (RewriteResult circuit1 0 True)
-                              (simplify circuit1 []))
+test2 = TestCase $ assertEqual "Support for empty lists of operations"
+                               (RewriteResult circ1 0 True) (simplify circ1 [])
 
 op1 :: RewriteOp
 op1 = RewriteOp (RewriteRule [] [x1, x1]) 0 True
 
-test3 :: Test
-test3 = TestCase (assertEqual "Support for rewrites of an empty circuits"
-                              (RewriteResult circuit1 1 True)
-                              (simplify [] [op1]))
+test3 = TestCase $ assertEqual "Support for rewrites of an empty circuits"
+                               (RewriteResult circ1 1 True) (simplify [] [op1])
 
 -----------------------------------------------------------------------------------------
 -- Edge cases: failure.
@@ -62,76 +58,66 @@ test3 = TestCase (assertEqual "Support for rewrites of an empty circuits"
 op2 :: RewriteOp
 op2 = RewriteOp (RewriteRule [x1, x2] [x1, x2]) 0 True
 
-test4 :: Test
-test4 = TestCase (assertEqual "Rejects rewrite when input string is empty"
-                              (RewriteResult [] 0 False)
-                              (simplify [] [op2]))
+test4 = TestCase $ assertEqual "Rejects rewrite when input string is empty"
+                               (RewriteResult [] 0 False) (simplify [] [op2])
 
-circuit2 :: Circuit
-circuit2 = [x1, x1, x1, x1, x2, x2]
+circ2 :: Circuit
+circ2 = [x1, x1, x1, x1, x2, x2]
 
-op3 :: RewriteOp
-op3 = RewriteOp (RewriteRule [] [x2, x2]) 4 True
+oplist1 :: [RewriteOp]
+oplist1 = [(RewriteOp (RewriteRule [] [x1, x1]) 0 True),
+           (RewriteOp (RewriteRule [] [x2, x2]) 4 True),
+           (RewriteOp (RewriteRule [x1, x2] [x1, x2]) 3 True),
+           (RewriteOp (RewriteRule [x1, x2] [x1, x2]) 0 True)]
 
-op4 :: RewriteOp
-op4 = RewriteOp (RewriteRule [x1, x2] [x1, x2]) 3 True
-
-test5 :: Test
-test5 = TestCase (assertEqual "Reject rewrite after 3 steps"
-                              (RewriteResult circuit2 3 False)
-                              (simplify circuit1 [op1, op3, op4, op2]))
+test5 = TestCase $ assertEqual "Reject rewrite after 3 steps"
+                               (RewriteResult circ2 3 False) (simplify circ1 oplist1)
 
 -----------------------------------------------------------------------------------------
 -- Non-trivial simplification.
 
-circuit3a :: Circuit
-circuit3a = [cx12, x3, ccx123, k12, z2, k12, x3]
+circ3a :: Circuit
+circ3a = [cx12, x3, ccx123, k12, z2, k12, x3]
 
-circuit3b :: Circuit
-circuit3b = [cx13, cx12, x2, ccx123]
+circ3b :: Circuit
+circ3b = [cx13, cx12, x2, ccx123]
 
-oplist :: [RewriteOp]
-oplist = [(RewriteOp (RewriteRule [] [x3, x3]) 3 True),
-          (RewriteOp (RewriteRule [ccx123, x3] [x3, ccx123]) 2 True),
-          (RewriteOp (RewriteRule [] [x3, x3]) 1 False),
-          (RewriteOp (RewriteRule [z2, k12] [k12, x2]) 4 True),
-          (RewriteOp (RewriteRule [k12, k12] []) 3 True),
-          (RewriteOp (RewriteRule [x3, x2] [x2, x3]) 2 True),
-          (RewriteOp (RewriteRule [x3, x3] []) 3 True),
-          (RewriteOp (RewriteRule [cx13, cx13] []) 2 False),
-          (RewriteOp (RewriteRule [cx13, x2] [x2, cx13]) 3 True),
-          (RewriteOp (RewriteRule [x2, ccx123] [ccx123, cx13, x2]) 1 False),
-          (RewriteOp (RewriteRule [ccx123, cx13] [cx13, ccx123]) 2 True),
-          (RewriteOp (RewriteRule [x2, cx13] [cx13, x2]) 1 True),
-          (RewriteOp (RewriteRule [cx12, cx13] [cx13, cx12]) 0 True)]
+oplist2 :: [RewriteOp]
+oplist2 = [(RewriteOp (RewriteRule [] [x3, x3]) 3 True),
+           (RewriteOp (RewriteRule [ccx123, x3] [x3, ccx123]) 2 True),
+           (RewriteOp (RewriteRule [] [x3, x3]) 1 False),
+           (RewriteOp (RewriteRule [z2, k12] [k12, x2]) 4 True),
+           (RewriteOp (RewriteRule [k12, k12] []) 3 True),
+           (RewriteOp (RewriteRule [x3, x2] [x2, x3]) 2 True),
+           (RewriteOp (RewriteRule [x3, x3] []) 3 True),
+           (RewriteOp (RewriteRule [cx13, cx13] []) 2 False),
+           (RewriteOp (RewriteRule [cx13, x2] [x2, cx13]) 3 True),
+           (RewriteOp (RewriteRule [x2, ccx123] [ccx123, cx13, x2]) 1 False),
+           (RewriteOp (RewriteRule [ccx123, cx13] [cx13, ccx123]) 2 True),
+           (RewriteOp (RewriteRule [x2, cx13] [cx13, x2]) 1 True),
+           (RewriteOp (RewriteRule [cx12, cx13] [cx13, cx12]) 0 True)]
 
-test6 :: Test
-test6 = TestCase (assertEqual "Non-trivial rewrite success"
-                              (RewriteResult circuit3b 13 True)
-                              (simplify circuit3a oplist))
+test6 = TestCase $ assertEqual "Non-trivial rewrite success"
+                               (RewriteResult circ3b 13 True) (simplify circ3a oplist2)
 
-circuit4a :: Circuit
-circuit4a = [cx12, x3, ccx123, k12, z2, k12]
+circ4a :: Circuit
+circ4a = [cx12, x3, ccx123, k12, z2, k12]
 
-circuit4b :: Circuit
-circuit4b = [cx12, ccx123, x2, x3]
+circ4b :: Circuit
+circ4b = [cx12, ccx123, x2, x3]
 
-test7 :: Test
-test7 = TestCase (assertEqual "Non-trivial rewrite success"
-                              (RewriteResult circuit4b 6 False)
-                              (simplify circuit4a oplist))
+test7 = TestCase $ assertEqual "Non-trivial rewrite success"
+                               (RewriteResult circ4b 6 False) (simplify circ4a oplist2)
 
 -----------------------------------------------------------------------------------------
 -- Orchestrates tests.
 
-tests :: Test
-tests = TestList [TestLabel "AllEmpty" test1,
-                  TestLabel "EmptyRuleList" test2,
-                  TestLabel "EmptyCircuit" test3,
-                  TestLabel "FailAtStep0" test4,
-                  TestLabel "FailAtStep3" test5,
-                  TestLabel "NontrivSuccess" test6,
-                  TestLabel "NontrivFail" test7]
+tests = hUnitTestToTests $ TestList [TestLabel "AllEmpty" test1,
+                                     TestLabel "EmptyRuleList" test2,
+                                     TestLabel "EmptyCircuit" test3,
+                                     TestLabel "FailAtStep0" test4,
+                                     TestLabel "FailAtStep3" test5,
+                                     TestLabel "NontrivSuccess" test6,
+                                     TestLabel "NontrivFail" test7]
 
-main :: IO Counts
-main = runTestTT tests
+main = defaultMain tests
