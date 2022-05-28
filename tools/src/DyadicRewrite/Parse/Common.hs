65f6651b9297cdcd70823e92f65155f2e4e38653
scottwe <ar627383@dal.ca>
2022-05-28 17:18:32 -0400

    Fn to parse integers from strings

--- tools/src/DyadicRewrite/Parse/Common.hs
+++ tools/src/DyadicRewrite/Parse/Common.hs
@@ -3,6 +3,7 @@
 module DyadicRewrite.Parse.Common where
 
 import Data.Char
+import Data.Maybe
 import Data.List.Split (splitOneOf)
 
 -----------------------------------------------------------------------------------------
@@ -17,3 +18,19 @@ splitAtFirst checkChar str = if (checkChar (head str))
                              then let (pre, post) = splitAtFirst checkChar (tail str)
                                   in ((head str) : pre, post)
                              else ("", str)
+
+-- | Consumes an input string (str). Returns the largest non-negative integral prefix of
+-- str coverted to an integer.
+parseNatInt :: String -> Maybe (Int, String)
+parseNatInt str
+    | digitStr == "" = Nothing
+    | otherwise      = Just ((read digitStr :: Int), post)
+    where (digitStr, post) = splitAtFirst isDigit str
+
+-- | Consumes an input string (str). Returns the largest integral prefix of str coverted
+-- to an integer.
+parseInt :: String -> Maybe (Int, String)
+parseInt ('-' : str) = case (parseNatInt str) of
+                         Just (digit, post) -> Just ((-1) * digit, post)
+                         Nothing            -> Nothing
+parseInt str         = parseNatInt str
--- tools/test/DyadicRewrite-Parse-Common-Tests.hs
+++ tools/test/DyadicRewrite-Parse-Common-Tests.hs
@@ -33,6 +33,84 @@ test6 = TestCase (assertEqual "Splitting with non-trivial pred without a match."
                               (splitAtFirst (\x -> x `elem` "123") "abcabc123"))
 
 -----------------------------------------------------------------------------------------
+-- parseNatInt
+
+test7 = TestCase (assertEqual "parseNatInt cannot parse an empty string."
+                              Nothing
+                              (parseNatInt ""))
+
+test8 = TestCase (assertEqual "parseNatInt cannot parse an alphabetical string."
+                              Nothing
+                              (parseNatInt "abc"))
+
+test9 = TestCase (assertEqual "parseNatInt can parse positive integers."
+                              (Just (123, "") :: Maybe (Int, String))
+                              (parseNatInt "123"))
+
+test10 = TestCase (assertEqual "parseNatInt will only parse decimal strings."
+                               (Just (123, "abc") :: Maybe (Int, String))
+                               (parseNatInt "123abc"))
+
+test11 = TestCase (assertEqual "parseNatInt stops after first non-numeric character."
+                               Nothing
+                               (parseNatInt "abc123"))
+
+test12 = TestCase (assertEqual "parseNatInt cannot parse negative integers."
+                               Nothing
+                               (parseNatInt "-123"))
+
+test13 = TestCase (assertEqual "parseNatInt handles multiple zeros."
+                               (Just (0, "abc") :: Maybe (Int, String))
+                               (parseNatInt "00000abc"))
+
+-----------------------------------------------------------------------------------------
+-- parseInt
+
+test14 = TestCase (assertEqual "parseInt cannot parse an empty string."
+                               Nothing
+                               (parseInt ""))
+
+test15 = TestCase (assertEqual "parseInt cannot parse an alphabetical string."
+                               Nothing
+                               (parseInt "abc"))
+
+test16 = TestCase (assertEqual "parseInt can parse positive integers."
+                               (Just (123, "") :: Maybe (Int, String))
+                               (parseInt "123"))
+
+test17 = TestCase (assertEqual "parseInt will only parse decimal strings."
+                                (Just (123, "abc") :: Maybe (Int, String))
+                                (parseInt "123abc"))
+
+test18 = TestCase (assertEqual "parseInt stops after first non-numeric character."
+                                Nothing
+                                (parseInt "abc123"))
+
+test19 = TestCase (assertEqual "parseInt parses negative integers."
+                               (Just (-123, "") :: Maybe (Int, String))
+                               (parseInt "-123"))
+
+test20 = TestCase (assertEqual "parseInt handles multiple zeros."
+                               (Just (0, "abc") :: Maybe (Int, String))
+                               (parseInt "00000abc"))
+
+test21 = TestCase (assertEqual "parseInt handles negative zeros."
+                               (Just (0, "") :: Maybe (Int, String))
+                               (parseInt "-0"))
+
+test22 = TestCase (assertEqual "parseInt stops after first non-numeric character (neg)."
+                               (Just (-123, "abc") :: Maybe (Int, String))
+                               (parseInt "-123abc"))
+
+test23 = TestCase (assertEqual "parseInt rejects double negation."
+                               Nothing
+                               (parseInt "--123"))
+
+test24 = TestCase (assertEqual "parseInt leading rejects dashes outside of numbers."
+                               Nothing
+                               (parseInt "-abc"))
+
+-----------------------------------------------------------------------------------------
 -- Orchestrates tests.
 
 tests = hUnitTestToTests $ TestList [TestLabel "splitAtFirst_TruePred_EmptyStr" test1,
@@ -40,6 +118,24 @@ tests = hUnitTestToTests $ TestList [TestLabel "splitAtFirst_TruePred_EmptyStr" 
                                      TestLabel "splitAtFirst_TruePred" test3,
                                      TestLabel "splitAtFirst_FalsePred" test4,
                                      TestLabel "splitAtFirst_MatchingPred" test5,
-                                     TestLabel "splitAtFirst_NonMatchingPred" test6]
+                                     TestLabel "splitAtFirst_NonMatchingPred" test6,
+                                     TestLabel "parseNatInt_EmptyString" test7,
+                                     TestLabel "parseNatInt_abc" test8,
+                                     TestLabel "parseNatInt_123" test9,
+                                     TestLabel "parseNatInt_123abc" test10,
+                                     TestLabel "parseNatInt_abc123" test11,
+                                     TestLabel "parseNatInt_-123" test12,
+                                     TestLabel "parseNatInt_00000abc" test13,
+                                     TestLabel "parseInt_EmptyString" test14,
+                                     TestLabel "parseInt_abc" test15,
+                                     TestLabel "parseInt_123" test16,
+                                     TestLabel "parseInt_123abc" test17,
+                                     TestLabel "parseInt_abc123" test18,
+                                     TestLabel "parseInt_-123" test19,
+                                     TestLabel "parseInt_00000abc" test20,
+                                     TestLabel "parseInt_-0" test21,
+                                     TestLabel "parseInt_-123abc" test22,
+                                     TestLabel "parseInt_--123" test23,
+                                     TestLabel "parseInt_-abc" test24]
 
 main = defaultMain tests
