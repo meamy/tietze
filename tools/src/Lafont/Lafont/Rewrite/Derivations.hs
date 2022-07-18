e37d39a1f936eb82372110409cdbe65175dd2717
scottwe <ar627383@dal.ca>
2022-07-18 08:20:40 -0400

    Functions to build dependency graphs from derivations

--- tools/src/Lafont/Lafont/Rewrite/Derivations.hs
+++ tools/src/Lafont/Lafont/Rewrite/Derivations.hs
@@ -3,10 +3,7 @@
 
 module Lafont.Rewrite.Derivations where
 
-import qualified Data.Set
-import qualified Data.Map
-import Data.Maybe
-import Lafont.Maybe
+import Lafont.Graph
 import Lafont.Rewrite.Rules
 import Lafont.Rewrite.Summary
 
@@ -17,3 +14,67 @@ import Lafont.Rewrite.Summary
 data Derivation = Derivation { summary :: DerivationSummary
                              , rewrites :: [Rewrite]
                              } deriving (Eq,Show)
+
+-----------------------------------------------------------------------------------------
+-- * Derivation to Graph Conversion
+
+-- | A vertex in a dependency graph.
+type Dependency = String
+
+-- | A graph to store dependencies between derivations.
+type DepGraph = Digraph Dependency
+
+-- | Consumes a list of derivations (list). Returns a dependency graph with a vertex for
+-- each derivation in list. A special vertex is added for unnamed derivations ("").
+registerDerivations :: [Derivation] -> DepGraph
+registerDerivations []                = addVertex nullgraph ""
+registerDerivations (derivation:list) =
+    case (propName (meta (summary derivation))) of
+        Just name -> addVertex g name
+        Nothing   -> g
+    where g = registerDerivations list
+
+-- | Consumes a dependency (dep), a rewrite, and a dependency graph (g). If the rewrite
+-- applies a derivation (dep') and at least one of dep or dep' is not a vertex in g, then
+-- dep' is returned as an error. If both dep and dep' are in g, then the graph obtained
+-- by adding an edge from dep to dep' is returned. If rewrite does not apply a
+-- derivation, then g is returned.
+addDepToGraph :: Dependency -> Rewrite -> DepGraph -> Either Dependency DepGraph
+addDepToGraph src rewrite g =
+    case (derivedFrom (rule rewrite)) of
+        Just dep -> case (addEdge g src dep) of
+            Just g' -> Right g'
+            Nothing -> Left dep
+        Nothing -> Right g
+
+-- | Consumes a dependency (dep), a list of rewrites (rewrites), and a dependency graph
+-- (g). If there exists a rewrite (r) in rewrites such that (addDepToGraph dep r g)
+-- returns an error, then the first such error is returned. Otherwise, is equivalent to
+-- folding rewrites using (addDepToGraph dep).
+addDepsToGraph :: Dependency -> [Rewrite] -> DepGraph -> Either Dependency DepGraph
+addDepsToGraph _   []                 g = Right g
+addDepsToGraph src (rewrite:rewrites) g = 
+    case (addDepToGraph src rewrite g) of
+        Left dep -> Left dep
+        Right g' -> addDepsToGraph src rewrites g'
+
+-- | Consumes a derivation and a graph. If the derivation is named, then returns the
+-- result of addDepsToGraph using the name and rewrites of the derivation. Otherwise,
+-- returns the results of addDepsToGraph using the name "" and the rewrites of the
+-- derivation.
+addDerivationToGraph :: Derivation -> DepGraph -> Either Dependency DepGraph
+addDerivationToGraph derivation g = 
+    case (propName (meta (summary derivation))) of
+        Just src -> addDepsToGraph src (rewrites derivation) g
+        Nothing  -> addDepsToGraph "" (rewrites derivation) g
+
+-- | Consumes a list of derivations (derivations) and a graph (g). If there exists a
+-- derivation (d) in rewrites such that (addDerivationToGraph d g) returns an error, then
+-- the first such error is returns. Otherwise, is equivalent to folding derivations using
+-- addDerivationToGraph.
+addDerivationsToGraph :: [Derivation] -> DepGraph -> Either Dependency DepGraph
+addDerivationsToGraph []                       g = Right g
+addDerivationsToGraph (derivation:derivations) g =
+    case (addDerivationToGraph derivation g) of
+        Left dep -> Left dep
+        Right g' -> addDerivationsToGraph derivations g'
---
+++ tools/test/Lafont-Rewrite-Derivations-Tests.hs
@@ -0,0 +1,221 @@
+module Main where
+
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Test.HUnit
+import Data.Maybe
+import Lafont.Graph
+import Lafont.Rewrite.Derivations
+import Lafont.Rewrite.Rules
+import Lafont.Rewrite.Summary
+
+-----------------------------------------------------------------------------------------
+-- Declares derivations for tests.
+
+relx = "unknown"
+rel1 = "rel1"
+rel2 = "rel2"
+rel3 = "rel3"
+rel4 = "rel4"
+rel5 = "rel5"
+
+summaryo = DerivationSummary (RewritePreamble Nothing) [] []
+summary1 = DerivationSummary (RewritePreamble (Just rel1)) [] []
+summary2 = DerivationSummary (RewritePreamble (Just rel2)) [] []
+summary3 = DerivationSummary (RewritePreamble (Just rel3)) [] []
+summary4 = DerivationSummary (RewritePreamble (Just rel4)) [] []
+summary5 = DerivationSummary (RewritePreamble (Just rel5)) [] []
+
+rwto = Rewrite (RewriteRule [] [] True Nothing) 0 True
+rwtx = Rewrite (RewriteRule [] [] True (Just relx)) 0 True
+rwt1 = Rewrite (RewriteRule [] [] True (Just rel1)) 0 True
+rwt2 = Rewrite (RewriteRule [] [] True (Just rel2)) 0 True
+rwt3 = Rewrite (RewriteRule [] [] True (Just rel3)) 0 True
+rwt4 = Rewrite (RewriteRule [] [] True (Just rel4)) 0 True
+rwt5 = Rewrite (RewriteRule [] [] True (Just rel5)) 0 True
+
+derivationo = Derivation summaryo [rwto, rwto, rwto, rwto, rwto, rwto]
+derivation1 = Derivation summaryo [rwto, rwt1, rwto, rwto, rwt3, rwto]
+derivationx = Derivation summaryo [rwto, rwto, rwto, rwtx, rwto, rwto]
+
+namedDerivation1 = Derivation summary1 [rwto, rwto, rwto, rwto, rwto, rwto]
+namedDerivation2 = Derivation summary2 [rwto, rwt1, rwt1, rwto, rwt3, rwto]
+namedDerivation3 = Derivation summary3 [rwto, rwto, rwto, rwt1, rwto, rwto]
+namedDerivation4 = Derivation summary4 [rwto, rwto, rwto, rwto, rwto, rwt3]
+namedDerivation5 = Derivation summary5 [rwt3, rwto, rwt4, rwto, rwto, rwto]
+namedDerivationx = Derivation summary1 [rwto, rwto, rwto, rwtx, rwto, rwto]
+namedDerivationc = Derivation summary1 [rwto, rwto, rwto, rwto, rwto, rwt2]
+
+list0 = []
+list1 = namedDerivation1:list0
+list2 = namedDerivation2:list1
+list3 = [namedDerivation3, namedDerivation4, namedDerivation5] ++ list2
+list4 = derivationo:list3
+list5 = derivation1:list4
+
+-----------------------------------------------------------------------------------------
+-- registerDerivations
+
+reg0 = addVertex nullgraph ""
+reg1 = addVertex reg0 rel1
+reg2 = addVertex reg1 rel2
+reg3 = addVertex (addVertex (addVertex reg2 rel3) rel4) rel5
+
+test1 = TestCase (assertEqual "An empty list of derivation registers a null graph."
+                  reg0
+                  (registerDerivations list0))
+
+test2 = TestCase (assertEqual "A single named derivation registers one vertex."
+                  reg1
+                  (registerDerivations list1))
+
+test3 = TestCase (assertEqual "Two named derivation register two vertices."
+                  reg2
+                  (registerDerivations list2))
+
+test4 = TestCase (assertEqual "Five named derivation register five vertices."
+                  reg3
+                  (registerDerivations list3))
+
+test5 = TestCase (assertEqual "Unnamed derivations are not added to graphs."
+                  reg3
+                  (registerDerivations list4))
+
+-----------------------------------------------------------------------------------------
+-- addDepToGraph
+
+reg4 = fromJust $ addEdge reg3 rel2 rel1
+reg5 = fromJust $ addEdge reg4 rel2 rel3
+
+test6 = TestCase (assertEqual "Applying a known derivation is allowed (1/2)."
+                  (Right reg4)
+                  (addDepToGraph rel2 rwt1 reg3))
+
+test7 = TestCase (assertEqual "Applying a known derivation is allowed (2/2)."
+                  (Right reg5)
+                  (addDepToGraph rel2 rwt3 reg4))
+
+test8 = TestCase (assertEqual "Using a relation is a no-ops."
+                  (Right reg5)
+                  (addDepToGraph rel2 rwto reg5))
+
+test9 = TestCase (assertEqual "Apply must be used on a known relation."
+                  (Left relx)
+                  (addDepToGraph rel2 rwtx reg5))
+
+-----------------------------------------------------------------------------------------
+-- addDepsToGraph
+
+test10 = TestCase (assertEqual "addDepsToGraph supports empty lists of rewrites"
+                   (Right reg3)
+                   (addDepsToGraph rel2 [] reg3))
+
+test11 = TestCase (assertEqual "addDepsToGraph supports sinlgeton rewrite lists."
+                   (Right reg4)
+                   (addDepsToGraph rel2 [rwt1] reg3))
+
+test12 = TestCase (assertEqual "addDepsToGraph supports rewrite pairs."
+                   (Right reg5)
+                   (addDepsToGraph rel2 [rwt1, rwt3] reg3))
+
+test13 = TestCase (assertEqual "addDepsToGraph supports no-ops."
+                   (Right reg5)
+                   (addDepsToGraph rel2 [rwto, rwt1, rwto, rwt3, rwto] reg3))
+
+test14 = TestCase (assertEqual "addDepsToGraph requires relations are known.."
+                   (Left relx)
+                   (addDepsToGraph rel2 [rwto, rwt1, rwto, rwt3, rwto, rwtx] reg5))
+
+-----------------------------------------------------------------------------------------
+-- addDerivationToGraph
+
+reg6 = fromJust $ addEdge reg5 rel3 rel1
+
+test15 = TestCase (assertEqual "addDerivationToGraph supports unnamed derivations."
+                   (Right reg3)
+                   (addDerivationToGraph derivationo reg3))
+
+test16 = TestCase (assertEqual "addDerivationToGraph rejects invalid unnamed derivations."
+                   (Left relx)
+                   (addDerivationToGraph derivationx reg3))
+
+test17 = TestCase (assertEqual "addDerivationToGraph supports dep-free derivations."
+                   (Right reg3)
+                   (addDerivationToGraph namedDerivation1 reg3))
+
+test18 = TestCase (assertEqual "addDerivationToGraph supports valid derivations (1/2)."
+                   (Right reg5)
+                   (addDerivationToGraph namedDerivation2 reg3))
+
+test19 = TestCase (assertEqual "addDerivationToGraph supports valid derivations (2/2)."
+                   (Right reg6)
+                   (addDerivationToGraph namedDerivation3 reg5))
+
+test20 = TestCase (assertEqual "addDerivationToGraph rejects invalid derivations."
+                   (Left relx)
+                   (addDerivationToGraph namedDerivationx reg5))
+
+-----------------------------------------------------------------------------------------
+-- addDerivationsToGraph
+
+reg7 = fromJust $ addEdge reg6 rel4 rel3
+reg8 = fromJust $ addEdge reg7 rel5 rel3
+reg9 = fromJust $ addEdge reg8 rel5 rel4
+reg10 = fromJust $ addEdge reg9 "" rel1
+reg11 = fromJust $ addEdge reg10 "" rel3
+
+test21 = TestCase (assertEqual "Can add edges for derivations in list0."
+                   (Right reg3)
+                   (addDerivationsToGraph list0 reg3))
+
+test22 = TestCase (assertEqual "Can add edges for derivations in list1."
+                   (Right reg3)
+                   (addDerivationsToGraph list1 reg3))
+
+test23 = TestCase (assertEqual "Can add edges for derivations in list2."
+                   (Right reg5)
+                   (addDerivationsToGraph list2 reg3))
+
+test24 = TestCase (assertEqual "Can add edges for derivations in list3."
+                   (Right reg9)
+                   (addDerivationsToGraph list3 reg3))
+
+test25 = TestCase (assertEqual "Can add edges for derivations in list4."
+                   (Right reg9)
+                   (addDerivationsToGraph list4 reg3))
+
+test26 = TestCase (assertEqual "Unnamed derivations are still recorded in the graph."
+                   (Right reg11)
+                   (addDerivationsToGraph list5 reg3))
+
+-----------------------------------------------------------------------------------------
+-- Orchestrates tests.
+
+tests = hUnitTestToTests $ TestList [TestLabel "registerDerived_Empty" test1,
+                                     TestLabel "registerDerived_Single" test2,
+                                     TestLabel "registerDerived_Double" test3,
+                                     TestLabel "registerDerived_Many" test4,
+                                     TestLabel "registerDerived_Unnamed" test5,
+                                     TestLabel "addDepToGraph_Valid1" test6,
+                                     TestLabel "addDepToGraph_Valid2" test7,
+                                     TestLabel "addDepToGraph_Noop" test8,
+                                     TestLabel "addDepToGraph_BadApply" test9,
+                                     TestLabel "addDepsToGraph_Empty" test10,
+                                     TestLabel "addDepsToGraph_Single" test11,
+                                     TestLabel "addDepsToGraph_Double" test12,
+                                     TestLabel "addDepsToGraph_Noop" test13,
+                                     TestLabel "addDepsToGraph_MissingDeriv" test14,
+                                     TestLabel "addDerivationToGraph_Unnamed" test15,
+                                     TestLabel "addDerivationToGraph_UnnamedX" test16,
+                                     TestLabel "addDerivationToGraph_Noop" test17,
+                                     TestLabel "addDerivationToGraph_Valid1" test18,
+                                     TestLabel "addDerivationToGraph_Valid2" test19,
+                                     TestLabel "addDerivationToGraph_Missing" test20,
+                                     TestLabel "addDerivationsToGraph_Empty" test21,
+                                     TestLabel "addDerivationsToGraph_Singleton" test22,
+                                     TestLabel "addDerivationsToGraph_Double" test23,
+                                     TestLabel "addDerivationsToGraph_Many" test24,
+                                     TestLabel "addDerivationsToGraph_Unnamed1" test25,
+                                     TestLabel "addDerivationsToGraph_Unnamed2" test26]
+
+main = defaultMain tests
