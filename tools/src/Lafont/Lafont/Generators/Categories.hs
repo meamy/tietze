28dda945be7dec92f6552a4ed32953a1a346ac2e
scottwe <ar627383@dal.ca>
2022-08-04 18:13:36 -0400

    Introduced class to represent monoid objects

---
+++ tools/src/Lafont/Lafont/Generators/Categories.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+-- | Provides data types for the semantics of categories, their generalizations (e.g.
+-- monoidal categories) and their specializations (e.g., monoids).
+
+module Lafont.Generators.Categories where
+
+import Quantum.Synthesis.Ring
+import Quantum.Synthesis.Matrix
+
+-----------------------------------------------------------------------------------------
+-- * Single Abstract Categories (Abstract Algebraic Types).
+
+-- | Additive Integers: the abelian group of integers under addition.
+data AddInt = AddInt Int deriving (Eq,Show)
+
+-- | Multiplicative Integers: the commutative monoid of integers under multiplication.
+data MultInt = MultInt Int deriving (Eq,Show)
+
+-- | A monoid in the sense of abstract algebra. Typically, this class describes a set of
+-- elements that are equipped with an association composition operator and an identity
+-- constant. More generally, this class describes a monoid object in a monoidal category.
+-- When M is monoid object in the Category of Sets with Cartesian product, the two
+-- definitions coincide.
+class (Eq a) => MonoidObj a where
+    compose :: a -> a -> a
+    identity :: a
+
+instance MonoidObj MultInt where
+    compose (MultInt x) (MultInt y) = MultInt (x * y)
+    identity                        = MultInt 1
+
+instance MonoidObj AddInt where
+    compose (AddInt x) (AddInt y) = AddInt (x + y)
+    identity                      = AddInt 0
+
+instance MonoidObj (Matrix Four Four Dyadic) where
+    compose a b = a * b
+    identity    = fromInteger 1
+
+instance MonoidObj (Matrix Eight Eight Dyadic) where
+    compose a b = a * b
+    identity    = fromInteger 1
---
+++ tools/test/Lafont-Generators-Categories-Tests.hs
@@ -0,0 +1,92 @@
+module Main where
+
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Test.HUnit
+import Quantum.Synthesis.Ring
+import Quantum.Synthesis.Matrix
+import Lafont.Generators.Categories
+import Lafont.Generators.QubitGates
+
+-----------------------------------------------------------------------------------------
+-- Additive Integers (Monoid).
+
+test1 = TestCase (assertEqual "Can determine the identity of the additive integers."
+                              (AddInt 0)
+                              (identity :: AddInt))
+
+test2 = TestCase (assertEqual "Can compose additive integers (1/2)."
+                              (AddInt 12)
+                              (compose (AddInt 5) (AddInt 7)))
+
+test3 = TestCase (assertEqual "Can compose additive integers (2/2)."
+                              (AddInt 76)
+                              (compose (AddInt 4) (AddInt 72)))
+
+-----------------------------------------------------------------------------------------
+-- Multiplicative Integers (Monoid).
+
+test4 = TestCase (assertEqual "Can determine the identity of the multiplicative integers."
+                              (MultInt 1)
+                              (identity :: MultInt))
+
+test5 = TestCase (assertEqual "Can compose multiplicative integers (1/2)."
+                              (MultInt 35)
+                              (compose (MultInt 5) (MultInt 7)))
+
+test6 = TestCase (assertEqual "Can compose multiplicative integers (2/2)."
+                              (MultInt 40)
+                              (compose (MultInt 4) (MultInt 10)))
+
+-----------------------------------------------------------------------------------------
+-- Four-by-four Dyadic Matrices (Monoid).
+
+test7 = TestCase (assertEqual "Can determine the identity of the 4x4 dyadic matrices."
+                              (fromInteger 1 :: Matrix Four Four Dyadic)
+                              (identity :: Matrix Four Four Dyadic))
+
+test8 = TestCase (assertEqual "Can compose 4x4 dyadic matrices (1/2)."
+                              (gate_cx * gate :: Matrix Four Four Dyadic)
+                              (compose gate_cx gate))
+    where gate = gate_x `tensor` gate_z
+
+test9 = TestCase (assertEqual "Can compose 4x4 dyadic matrices (2/2)."
+                              (gate_swap * gate_k :: Matrix Four Four Dyadic)
+                              (compose gate_swap gate_k))
+
+
+-----------------------------------------------------------------------------------------
+-- Eight-by-eight Dyadic Matrices (Monoid).
+
+test10 = TestCase (assertEqual "Can determine the identity of the 8x8 dyadic matrices."
+                               (fromInteger 1 :: Matrix Eight Eight Dyadic)
+                               (identity :: Matrix Eight Eight Dyadic))
+
+test11 = TestCase (assertEqual "Can compose 8x8 dyadic matrices (1/2)."
+                               (gate_tof * gate :: Matrix Eight Eight Dyadic)
+                               (compose gate_tof gate))
+    where gate = gate_x `tensor` gate_k
+
+test12 = TestCase (assertEqual "Can compose 8x8 dyadic matrices (2/2)."
+                               (gate1 * gate2 :: Matrix Eight Eight Dyadic)
+                               (compose gate1 gate2))
+    where gate1 = gate_x `tensor` gate_cx
+          gate2 = gate_k `tensor` gate_z
+
+-----------------------------------------------------------------------------------------
+-- Orchestrates tests.
+
+tests = hUnitTestToTests $ TestList [TestLabel "AddInt_Identity" test1,
+                                     TestLabel "AddInt_Compose_1" test2,
+                                     TestLabel "AddInt_Compose_2" test3,
+                                     TestLabel "MultInt_Identity" test4,
+                                     TestLabel "MultInt_Compose_1" test5,
+                                     TestLabel "MultInt_Compose_2" test6,
+                                     TestLabel "4x4Dyadic_Identity" test7,
+                                     TestLabel "4x4Dyadic_Compose_1" test8,
+                                     TestLabel "4x4Dyadic_Compose_2" test9,
+                                     TestLabel "8x8Dyadic_Identity" test10,
+                                     TestLabel "8x8Dyadic_Compose_1" test11,
+                                     TestLabel "8x8Dyadic_Compose_2" test12]
+
+main = defaultMain tests
