deb2401ca2652e0ff0d5509efaea7e0c7acf661d
scottwe <aswesley@uwaterloo.ca>
2023-04-06 14:35:34 -0300

    Added run script for E8 - not portable

---
+++ tools/examples/derivations/run.sh
@@ -0,0 +1,17 @@
+../../dist-newstyle/build/x86_64-linux/ghc-8.8.4/Lafont-0.1.0.0/x/validate_derivations/build/validate_derivations/validate_derivations \
+    generators.gens \
+    relations.rel \
+    aux/lemmas.derivs \
+    aux/orders.derivs \
+    aux/symmetry.derivs \
+    aux/bifunct.derivs \
+    aux/spatial.derivs \
+    aux/misc.derivs \
+    s1s1.derivs s1s2.derivs s1s3.derivs s1s4.derivs s1s5.derivs s1s6.derivs s1s7.derivs s1s8.derivs \
+    s2s2.derivs s2s3.derivs s2s4.derivs s2s5.derivs s2s6.derivs s2s7.derivs s2s8.derivs \
+    s3s3.derivs s3s4.derivs s3s5.derivs s3s6.derivs s3s7.derivs s3s8.derivs \
+    s4s4.derivs s4s5.derivs s4s6.derivs s4s7.derivs s4s8.derivs \
+    s5s5.derivs s5s6.derivs s5s7.derivs s5s8.derivs \
+    s6s6.derivs s6s7.derivs s6s8.derivs \
+    s7s7.derivs s7s8.derivs \
+    s8s8.derivs
