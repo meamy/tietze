f9ac71d674e97b01df3de5e851bf55b9a46734d9
scottwe <scott.wesley@dal.ca>
2024-02-16 16:03:13 -0400

    LaTeX macros for relation symbols

--- tools/src/Lafont/Lafont/Format/LaTeX.hs
+++ tools/src/Lafont/Lafont/Format/LaTeX.hs
@@ -1,61 +1,77 @@
 -- | Provides an interface to format derivations in LaTeX.
 
 module Lafont.Format.LaTeX (
-    LaTeX ( .. ),
-    GenMacroList,
-    makeGenMacros
+    MacroList,
+    makeGenMacros,
+    makeRelMacros,
+    printMacroList
 ) where
 
 import qualified Data.Map                    as Map
 import           Lafont.Common
+import           Lafont.Graph
 import           Lafont.Generators.Semantics
 import           Lafont.Rewrite.Derivations
+import           Lafont.Rewrite.Lookup
 import           Lafont.Rewrite.Rules
 import           Lafont.Rewrite.Summary
 
 -----------------------------------------------------------------------------------------
--- * General Interface for Latex Components.
-
--- | Represents a LaTeX document component.
-class LaTeX a where
-    -- | Prints the component as LaTeX source code.
-    toLaTeX :: a -> String
-
------------------------------------------------------------------------------------------
 -- * GenMacro Dictionary.
 
--- | Describes a newcommand definition which associates a generator symbol with its math
--- mode representation. For example, (GenMacroDef lft_SWAP01 X_0) maps the generator
+-- | Describes a newcommand definition which associates a derivational symbol with its
+-- math mode representation. For example, (MacroDef lft_SWAP01 X_0) maps the generator
 -- SWAP01 to the symbol X_0 through a macro called lft_SAWP01.
-data GenMacroDef = GenMacroDef String String deriving (Eq, Show)
+data MacroDef = MacroDef String String deriving (Eq, Show)
 
--- | Maintains a mapping from generator names to macro definitions. Each macro definition
--- allows an end-user to decide the symbol associated with each generator in the LaTeX
--- depiction of a derivation. In terms of LaTeX source code, this component is a list of
--- newcommand statements.
-newtype GenMacroList = GenMacroList (Map.Map String GenMacroDef) deriving (Eq, Show)
+-- | Maintains a mapping from derivational symbol names to macro definitions. Each macro
+-- definition allows an end-user to decide the math mode representation associated with
+-- each symbol in the LaTeX depiction of a derivation. In terms of LaTeX source code,
+-- this component is a list of newcommand statements.
+newtype MacroList = MacroList (Map.Map String MacroDef) deriving (Eq, Show)
+
+-- | Prints a single macro definition as a newcommand statement.
+printMacroDef :: MacroDef -> String
+printMacroDef (MacroDef cmd sym) = "\\newcommand{" ++ cmd ++ "}{" ++ sym ++ "}"
+
+-- | Prints a MacroList as a list of newcommand statements, which each statement has a
+-- comment relating the macro to its designated generator symbol.
+printMacroList :: MacroList -> String
+printMacroList (MacroList map) = Map.foldrWithKey f "" map
+    where f gen def res = let comment = "% Macro for: " ++ gen 
+                          in comment ++ "\n" ++ printMacroDef def ++ "\n" ++ res
 
 -- | Implementation details for makeGenMacros. Consumes the alphabet corresponding to a
--- generator dictionary. Returns the raw map encapsulated by a GenMacroDef.
-makeGenMacrosImpl :: [String] -> Map.Map String GenMacroDef
+-- generator dictionary. Returns the raw map encapsulated by a MacroDef.
+makeGenMacrosImpl :: [String] -> Map.Map String MacroDef
 makeGenMacrosImpl []         = Map.empty
 makeGenMacrosImpl (gen:gens) = Map.insert gen def map
     where map = makeGenMacrosImpl gens
           idx = Map.size map
           cmd = "\\lftgen" ++ show idx
           sym = "X_{" ++ show idx ++ "}"
-          def = GenMacroDef cmd sym
+          def = MacroDef cmd sym
 
--- | Constructs the GenMacroList corresponding to a generator dictionary. Note that for
--- each LaTeX document, only one GenMacroList should be generated. Otherwise, the macro
--- names or corresponding math mode symbols may conflict.
-makeGenMacros :: GenDict a -> GenMacroList
-makeGenMacros = GenMacroList . makeGenMacrosImpl . toAlphabet
+-- | Constructs the MacroList corresponding to a generator dictionary. Note that for each
+-- LaTeX document, only one MacroList should be generated. Otherwise, the macro names or
+-- corresponding math mode symbols may conflict.
+makeGenMacros :: GenDict a -> MacroList
+makeGenMacros = MacroList . makeGenMacrosImpl . toAlphabet
 
-instance LaTeX GenMacroDef where
-    toLaTeX (GenMacroDef cmd sym) = "\\newcommand{" ++ cmd ++ "}{" ++ sym ++ "}"
+-- | Implementation details for makeRelMacros. Consumes the list of rule names
+-- corresponding to a rule dictionary. Returns the raw map encapsulated by a MacroDef.
+makeRelMacrosImpl :: [String] -> Map.Map String MacroDef
+makeRelMacrosImpl []         = Map.empty
+makeRelMacrosImpl (rel:rels) = Map.insert rel def map
+    where map = makeRelMacrosImpl rels
+          idx = Map.size map
+          cmd = "\\lftrel" ++ show idx
+          sym = "R_{" ++ show idx ++ "}"
+          def = MacroDef cmd sym
 
-instance LaTeX GenMacroList where
-    toLaTeX (GenMacroList map) = Map.foldrWithKey f "" map
-        where f gen def res = let comment = "% Macro for: " ++ gen 
-                              in comment ++ "\n" ++ toLaTeX def ++ "\n" ++ res
+-- | Constructs the MacroList corresponding to a rule dictionary. Assumes that all
+-- derived relations are included in the dictionary. Note that for each LaTeX document,
+-- only one MacroList should be generated. Otherwise, the macro names or corresponding
+-- math mode symbols may conflict.
+makeRelMacros :: RuleDict -> MacroList
+makeRelMacros = MacroList . makeRelMacrosImpl . toRuleNames
--- tools/test/Lafont-Format-LaTeX-Tests.hs
+++ tools/test/Lafont-Format-LaTeX-Tests.hs
@@ -1,13 +1,34 @@
 module Main where
 
-import Test.Framework
-import Test.Framework.Providers.HUnit
-import Test.HUnit
-import Lafont.Format.LaTeX
-import Lafont.Generators.Semantics
+import           Test.Framework
+import           Test.Framework.Providers.HUnit
+import           Test.HUnit
+import           Lafont.Common
+import           Lafont.Format.LaTeX
+import qualified Lafont.Generators.Semantics as Sem
+import qualified Lafont.Rewrite.Lookup as Rel
+import           Lafont.Rewrite.Rules
 
 -----------------------------------------------------------------------------------------
--- GenMacroList
+-- Declares some symbols to ues throughout the test.
+
+sym1 :: Symbol
+sym1 = Symbol "abc_xyz" []
+
+sym2 :: Symbol
+sym2 = Symbol "gen0" []
+
+sym3 :: Symbol
+sym3 = Symbol "gen1" []
+
+sym4 :: Symbol
+sym4 = Symbol "gen2" []
+
+sym5 :: Symbol
+sym5 = Symbol "gen3" []
+
+-----------------------------------------------------------------------------------------
+-- makeGenMacros
 
 gen1 :: (String, Maybe Int)
 gen1 = ("abc_xyz", Just 1)
@@ -18,33 +39,83 @@ gen2 = ("gen0", Just 2)
 gen3 :: (String, Maybe Int)
 gen3 = ("gen1", Nothing)
 
-sampleDict0 :: GenDict Int
-sampleDict0 = empty
-sampleDict1 = addGen sampleDict0 gen1
-sampleDict2 = addGen sampleDict1 gen2
-sampleDict3 = addGen sampleDict2 gen3
+gen4 :: (String, Maybe Int)
+gen4 = ("gen2", Nothing)
 
-test1 = TestCase (assertEqual "Can produce a empty GenMacroList from a GenDict."
+gen5 :: (String, Maybe Int)
+gen5 = ("gen3", Nothing)
+
+sampleGDict0 :: Sem.GenDict Int
+sampleGDict0 = Sem.empty
+sampleGDict1 = Sem.addGen sampleGDict0 gen1
+sampleGDict2 = Sem.addGen sampleGDict1 gen2
+sampleGDict3 = Sem.addGen sampleGDict2 gen3
+sampleGDict4 = Sem.addGen sampleGDict3 gen4
+sampleGDict5 = Sem.addGen sampleGDict4 gen5
+
+test1 = TestCase (assertEqual "Can produce a empty MacroList from a GenDict."
                               ""
-                              (toLaTeX $ makeGenMacros sampleDict0))
+                              (printMacroList $ makeGenMacros sampleGDict0))
 
-test2 = TestCase (assertEqual "Can produce a GenMacroList for a single generator."
+test2 = TestCase (assertEqual "Can produce a MacroList for a single generator."
                               (cmd1 ++ "\n")
-                              (toLaTeX $ makeGenMacros sampleDict1))
+                              (printMacroList $ makeGenMacros sampleGDict1))
     where cmd1 = "% Macro for: abc_xyz" ++ "\n" ++ "\\newcommand{\\lftgen0}{X_{0}}"
 
-test3 = TestCase (assertEqual "Can produce a GenMacroList for a set of generators."
+test3 = TestCase (assertEqual "Can produce a MacroList for a set of generators."
                               (cmd1 ++ "\n" ++ cmd2 ++ "\n" ++ cmd3 ++ "\n")
-                              (toLaTeX $ makeGenMacros sampleDict3))
+                              (printMacroList $ makeGenMacros sampleGDict3))
     where cmd1 = "% Macro for: abc_xyz" ++ "\n" ++ "\\newcommand{\\lftgen2}{X_{2}}"
           cmd2 = "% Macro for: gen0" ++ "\n" ++ "\\newcommand{\\lftgen1}{X_{1}}"
           cmd3 = "% Macro for: gen1" ++ "\n" ++ "\\newcommand{\\lftgen0}{X_{0}}"
+
+-----------------------------------------------------------------------------------------
+-- makeRelMacros
+
+rule1 :: RewriteRule
+rule1 = RewriteRule [sym1, sym2, sym3] [sym4] True (Primitive "rel1")
+
+rule2 :: RewriteRule
+rule2 = RewriteRule [sym2, sym2, sym2] [] True (Primitive "rel2")
+
+rule3 :: RewriteRule
+rule3 = RewriteRule [sym4, sym5] [sym5, sym4] True (Primitive "rel3")
+
+rule4 :: RewriteRule
+rule4 = RewriteRule [sym3] [sym1, sym2, sym4, sym5] True (Derived $ Just "drel1")
+
+sampleRDict0 :: Rel.RuleDict
+sampleRDict0 = Rel.empty
+sampleRDict1 = Rel.addRule sampleRDict0 ("rel1", rule1)
+sampleRDict2 = Rel.addRule sampleRDict1 ("rel2", rule2)
+sampleRDict3 = Rel.addRule sampleRDict2 ("rel3", rule3)
+sampleRDict4 = Rel.addRule sampleRDict3 ("drel1", rule4)
+
+test4 = TestCase (assertEqual "Can produce a empty MacroList from a GenDict."
+                              ""
+                              (printMacroList $ makeRelMacros sampleRDict0))
+
+test5 = TestCase (assertEqual "Can produce a MacroList for a single relation."
+                              (cmd1 ++ "\n")
+                              (printMacroList $ makeRelMacros sampleRDict1))
+    where cmd1 = "% Macro for: rel1" ++ "\n" ++ "\\newcommand{\\lftrel0}{R_{0}}"
+
+test6 = TestCase (assertEqual "Can produce a MacroList for a set of relations."
+                              (cmd1 ++ "\n" ++ cmd2 ++ "\n" ++ cmd3 ++ "\n" ++ cmd4 ++ "\n")
+                              (printMacroList $ makeRelMacros sampleRDict4))
+    where cmd1 = "% Macro for: drel1" ++ "\n" ++ "\\newcommand{\\lftrel3}{R_{3}}"
+          cmd2 = "% Macro for: rel1" ++ "\n" ++ "\\newcommand{\\lftrel2}{R_{2}}"
+          cmd3 = "% Macro for: rel2" ++ "\n" ++ "\\newcommand{\\lftrel1}{R_{1}}"
+          cmd4 = "% Macro for: rel3" ++ "\n" ++ "\\newcommand{\\lftrel0}{R_{0}}"
 
 -----------------------------------------------------------------------------------------
 -- Orchestrates tests.
 
 tests = hUnitTestToTests $ TestList [TestLabel "makeGenMacros_Empty" test1,
                                      TestLabel "makeGenMacros_1Gen" test2,
-                                     TestLabel "makeGenMacros_3Gen" test3]
+                                     TestLabel "makeGenMacros_3Gen" test3,
+                                     TestLabel "makeRelMacros_Empty" test4,
+                                     TestLabel "makeRelMacros_1Rel" test5,
+                                     TestLabel "makeRelMacros_4Rel" test6]
 
 main = defaultMain tests
